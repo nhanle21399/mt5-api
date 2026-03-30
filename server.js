f381e31f4764d02c167ff10fc91d7bd463ba8f24
nhanle21399 <nhanle21399@gmail.com>
2026-03-31 05:31:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(express.text());
 
 let latestTrade = {};
 
 app.post("/update", (req, res) => {
-  latestTrade = req.body;
-  console.log("New trade:", latestTrade);
-  res.sendStatus(200);
+  try {
+    const data = typeof req.body === "string" ? JSON.parse(req.body) : req.body;
+    latestTrade = data;
+
+    console.log("New trade:", latestTrade);
+    res.sendStatus(200);
+  } catch (err) {
+    console.log("Parse error:", err);
+    res.sendStatus(400);
+  }
 });
 
 app.get("/trade", (req, res) => {
